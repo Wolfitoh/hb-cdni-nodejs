ffe86b020e7a808e05e16486bb2b476d01e8e695
Angel Garcia <dooag0120@gmail.com>
2024-01-23 15:02:18 -0500

    cambio BD

--- public/config/credentials-bd.js
+++ public/config/credentials-bd.js
@@ -1,25 +1,26 @@
-const { Client } = require('pg');
+const { Client } = require("pg");
 
 // Crea una instancia única de cliente PostgreSQL
 const globalClient = new Client({
-    user: 'zaflumic',
-    host: 'silly.db.elephantsql.com',
-    database: 'zaflumic',
-    password: 'G6BvDAab5fg09f9J2oqwJik6HKLPvLrz',
-    port: 5432,
-    ssl: {
-        rejectUnauthorized: false, // Esta línea permite la conexión sin validar el certificado SSL (¡no se recomienda en producción!)
-    }
+  user: "koyeb-adm",
+  host: "ep-soft-bush-53286164.eu-central-1.pg.koyeb.app",
+  database: "koyebdb",
+  password: "ylTjAm7HJ0cs",
+  port: 5432,
+  ssl: {
+    rejectUnauthorized: false, // Esta línea permite la conexión sin validar el certificado SSL (¡no se recomienda en producción!)
+  },
 });
 
 // Conecta el cliente una vez al iniciar la aplicación
-globalClient.connect()
-    .then(() => {
-        console.log('Conexión exitosa a PostgreSQL');
-    })
-    .catch(err => {
-        console.error('Error al conectar a PostgreSQL', err);
-    });
+globalClient
+  .connect()
+  .then(() => {
+    console.log("Conexión exitosa a PostgreSQL");
+  })
+  .catch((err) => {
+    console.error("Error al conectar a PostgreSQL", err);
+  });
 
 // Exporta la instancia global del cliente para que pueda ser utilizada en otros módulos
 module.exports = globalClient;
--- public/routes/auth.routes.js
+++ public/routes/auth.routes.js
@@ -1,78 +1,77 @@
-const client = require('../config/credentials-bd.js');
-const path = require('path');
-const bcrypt = require('bcrypt');
-
+const client = require("../config/credentials-bd.js");
+const path = require("path");
+const bcrypt = require("bcrypt");
 
 module.exports = function (app) {
-    app.get('/', (req, res) => {
-        res.redirect('/');
-    })
+  app.get("/", (req, res) => {
+    res.redirect("/");
+  });
 
-    app.get('/consulta', (req, res) => {
-        const filePath = path.join(__dirname, '..', 'consulta.html');
-        res.sendFile(filePath);
-    });
+  app.get("/consulta", (req, res) => {
+    const filePath = path.join(__dirname, "..", "consulta.html");
+    res.sendFile(filePath);
+  });
 
-    app.post('/check-credentials', async (req, res) => {
-        try {
-            const { dni, password } = req.body;
-            // Consulta SQL para obtener la contraseña almacenada del usuario
-            const query = 'SELECT dni, password FROM usuario WHERE dni = $1';
-            const values = [dni];
-            const result = await client.query(query, values);
+  app.post("/check-credentials", async (req, res) => {
+    try {
+      const { dni, password } = req.body;
+      // Consulta SQL para obtener la contraseña almacenada del usuario
+      const query = "SELECT dni, password FROM usuario WHERE dni = $1";
+      const values = [dni];
+      const result = await client.query(query, values);
 
-            bcrypt.hash('Pachitea', 10, (err, hash) => {
-                if (err) {
-                    console.error('Error al encriptar la contraseña:', err);
-                } else {
-                    console.log('Contraseña encriptada:', hash);
-                }
-            });
+      // bcrypt.hash('Pachitea', 10, (err, hash) => {
+      //     if (err) {
+      //         console.error('Error al encriptar la contraseña:', err);
+      //     } else {
+      //         console.log('Contraseña encriptada:', hash);
+      //     }
+      // });
 
-            // Verificar si se encontró un usuario
-            if (result.rows.length === 1) {
-                const hashedPassword = result.rows[0].password;
+      // Verificar si se encontró un usuario
+      if (result.rows.length === 1) {
+        const hashedPassword = result.rows[0].password;
 
-                // Comparar la contraseña proporcionada con la contraseña almacenada en la base de datos
-                bcrypt.compare(password, hashedPassword, (err, passwordMatch) => {
-                    if (err) {
-                        console.error('Error al comparar contraseñas:', err);
-                        res.status(500).send({
-                            success: false,
-                            message: 'Error al autenticar usuario.',
-                        });
-                    } else if (passwordMatch) {
-                        // Contraseña coincidente
-                        delete result.rows[0].password;
-                        res.status(200).send({
-                            success: true,
-                            message: 'Inicio de sesión exitoso.',
-                            user: result.rows[0],
-                        });
-                    } else {
-                        // Contraseña incorrecta
-                        res.status(404).send({
-                            success: false,
-                            message: 'DNI del usuario o contraseña incorrectos.',
-                        });
-                    }
-                });
-            } else {
-                // Usuario no encontrado
-                console.log("no hay");
-                res.status(404).send({
-                    success: false,
-                    message: 'DNI del usuario o contraseña incorrectos.',
-                });
-            }
-        } catch (error) {
-            // Error en la consulta o conexión a la base de datos
-            console.error('Error al autenticar usuario:', error);
-
+        // Comparar la contraseña proporcionada con la contraseña almacenada en la base de datos
+        bcrypt.compare(password, hashedPassword, (err, passwordMatch) => {
+          if (err) {
+            console.error("Error al comparar contraseñas:", err);
             res.status(500).send({
-                success: false,
-                message: 'Error al autenticar usuario.',
+              success: false,
+              message: "Error al autenticar usuario.",
             });
-        }
-    });
+          } else if (passwordMatch) {
+            // Contraseña coincidente
+            delete result.rows[0].password;
+            res.status(200).send({
+              success: true,
+              message: "Inicio de sesión exitoso.",
+              user: result.rows[0],
+            });
+          } else {
+            // Contraseña incorrecta
+            res.status(404).send({
+              success: false,
+              message: "DNI del usuario o contraseña incorrectos.",
+            });
+          }
+        });
+      } else {
+        // Usuario no encontrado
+        console.log("no hay");
+        res.status(404).send({
+          success: false,
+          message: "DNI del usuario o contraseña incorrectos.",
+        });
+      }
+    } catch (error) {
+      // Error en la consulta o conexión a la base de datos
+      console.error("Error al autenticar usuario:", error);
+
+      res.status(500).send({
+        success: false,
+        message: "Error al autenticar usuario.",
+      });
+    }
+  });
 };
